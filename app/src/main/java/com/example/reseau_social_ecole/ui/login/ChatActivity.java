07a059ed25489e9e2679a1c877646893b3009863
aerolito <cesar.augusto.rodriguez@umontreal.ca>
2021-04-17 17:09:10 -0400

    chat interface

--- app/src/main/java/com/example/reseau_social_ecole/ui/login/ChatActivity.java
+++ app/src/main/java/com/example/reseau_social_ecole/ui/login/ChatActivity.java
@@ -2,13 +2,16 @@ package com.example.reseau_social_ecole.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.reseau_social_ecole.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -18,11 +21,42 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
 
         final ImageView back = findViewById(R.id.b_backMessages);
+        final Button send = findViewById(R.id.b_send);
+        final TextInputEditText champTexte = findViewById(R.id.champ_input);
+
+
 
         back.setOnClickListener((View v) -> {
             Intent intent = new Intent(this, MessageActivity.class);
             startActivity(intent);
         });
+
+        send.setOnClickListener((View v) -> {
+            cacher(this);
+
+            champTexte.setText("");
+
+        });
+
+
+
+
+
+
+
+
+    }
+
+    // FROM STACK OVERFLOW
+    public static void cacher(Activity activity) {
+        InputMethodManager in = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //prendre la fenetre adequate
+        View vue = activity.getCurrentFocus();
+        // si aucune créer une nouvelle
+        if (vue == null) {
+            vue = new View(activity);
+        }
+        in.hideSoftInputFromWindow(vue.getWindowToken(), 0);
     }
 
 

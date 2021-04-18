8f2009d1a8c6af2e6bea890cb6c5bb3a31d38ff5
aerolito <cesar.augusto.rodriguez@umontreal.ca>
2021-04-18 02:00:55 -0400

    petits changements

--- app/src/main/java/com/example/CONNECTadora/ui/CONNECTadora/ContactsActivity.java
+++ app/src/main/java/com/example/CONNECTadora/ui/CONNECTadora/ContactsActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,6 +25,82 @@ public class ContactsActivity extends AppCompatActivity {
         final ImageView msg2 = findViewById(R.id.msg2);
         final ImageView msg3 = findViewById(R.id.msg3);
         final ImageView msg4 = findViewById(R.id.msg4);
+
+        final ImageView profPic0 = findViewById(R.id.profPic7);
+        final ImageView profPic1 = findViewById(R.id.profPic5);
+        final ImageView profPic2 = findViewById(R.id.profPicture);
+        final ImageView profPic3 = findViewById(R.id.profPic6);
+
+        final TextView tel0 = findViewById(R.id.tel1) ;
+        final TextView tel1 = findViewById(R.id.tel2) ;
+        final TextView tel2 = findViewById(R.id.tel3);
+        final TextView tel3 = findViewById(R.id.tel4) ;
+
+        final TextView name0 = findViewById(R.id.nameView1) ;
+        final TextView name1 = findViewById(R.id.nameView2) ;
+        final TextView name2 = findViewById(R.id.nameView3);
+        final TextView name3 = findViewById(R.id.nameView0) ;
+
+        name0.setOnClickListener((View v) -> {
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        });
+        name1.setOnClickListener((View v) -> {
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        });
+        name2.setOnClickListener((View v) -> {
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        });
+        name3.setOnClickListener((View v) -> {
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        });
+
+
+
+        profPic0.setOnClickListener((View v) -> {
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        });
+        profPic1.setOnClickListener((View v) -> {
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        });
+        profPic2.setOnClickListener((View v) -> {
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        });
+        profPic3.setOnClickListener((View v) -> {
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        });
+
+
+
+
+
+
+        tel0.setOnClickListener((View v) -> {
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        });
+
+        tel1.setOnClickListener((View v) -> {
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        });
+
+        tel2.setOnClickListener((View v) -> {
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        });
+
+        tel3.setOnClickListener((View v) -> {
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        });
 
         msg1.setOnClickListener((View v) -> {
             Intent intent = new Intent(this, ChatActivity.class);
@@ -65,4 +142,6 @@ public class ContactsActivity extends AppCompatActivity {
             startActivity(intent);
         });
     }
+
+
 }
